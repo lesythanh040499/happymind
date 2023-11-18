47f0abb13378b22e5704c9d912962ef58d70656b
Thanh <kentkillboss@gmail.com>
2023-11-18 16:51:06 +0700

    adadad1123

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import adminController from '../app/controllers/adminController.js';
+import adminController from '../app/controllers/AdminController.js';
 
 const router = express.Router();
 
