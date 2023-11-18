b919e16b38d982e2b06f918717224a29797cd2a8
Thanh <kentkillboss@gmail.com>
2023-11-18 16:23:48 +0700

    adad

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import express from 'express';
-import morgan from 'morgan';
-import { engine } from 'express-handlebars';
-import { route } from './routes/index.js';
-import { connect } from './config/db/index.js';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express from "express";
+import { engine } from "express-handlebars";
+import morgan from "morgan";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { connect } from "./config/db/index.js";
 
 dotenv.config();
 
@@ -16,30 +14,31 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Template engine
 app.engine(
-    'hbs',
-    engine({
-        extname: '.hbs',
-        helpers: {
-            sum: (a, b) => a + b,
-        },
-    }),
+  "hbs",
+  engine({
+    extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+    },
+  })
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+app.get("/", (req, res) => res.send("adad"));
 
 // Routes init
-route(app);
+// route(app);
 
 // Connect to DB
 connect();
 
 app.listen(port, () => {
-    console.log(`App listening on port http://localhost:${port}`);
+  console.log(`App listening on port http://localhost:${port}`);
 });
