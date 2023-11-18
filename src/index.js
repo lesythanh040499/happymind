880901fd8c2c7b6197fea79482720232e903dafb
Thanh <kentkillboss@gmail.com>
2023-11-18 16:41:38 +0700

    adadadada

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import express from "express";
-import { engine } from "express-handlebars";
-import morgan from "morgan";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import { connect } from "./config/db/index.js";
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { connect } from './config/db/index.js';
+import { route } from './routes/index.js';
 
 dotenv.config();
 
@@ -14,31 +15,30 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Template engine
 app.engine(
-  "hbs",
-  engine({
-    extname: ".hbs",
-    helpers: {
-      sum: (a, b) => a + b,
-    },
-  })
+    'hbs',
+    engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources", "views"));
-app.get("/", (req, res) => res.send("adad"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
-// route(app);
+route(app);
 
 // Connect to DB
 connect();
 
 app.listen(port, () => {
-  console.log(`App listening on port http://localhost:${port}`);
+    console.log(`App listening on port http://localhost:${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
+import adminRouter from './admin.js';
+import blogRouter from './blog.js';
+import blogdetailRouter from './blogdetail.js';
+import foodcaloriesRouter from './foodcalories.js';
+import homepageRouter from './homepage.js';
+import userRouter from './user.js';
+import videoRouter from './video.js';
+
 export function route(app) {
-  // app.use('/', homepageRouter);
-  // app.use('/video', videoRouter);
-  // app.use('/food', foodcaloriesRouter);
-  // app.use('/blog', blogRouter);
-  // app.use('/admin', adminRouter);
-  // app.use('/blogdetail', blogdetailRouter);
-  // app.use('/user', userRouter);
-  // app.use('/blog', blogRouter);
+    app.use('/', homepageRouter);
+    app.use('/video', videoRouter);
+    app.use('/food', foodcaloriesRouter);
+    app.use('/blog', blogRouter);
+    app.use('/admin', adminRouter);
+    app.use('/blogdetail', blogdetailRouter);
+    app.use('/user', userRouter);
+    app.use('/blog', blogRouter);
 }
